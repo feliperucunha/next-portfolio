09a48cb6f159a72af39548c974074eeed84b2551
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-08 14:32:45 -0300

    hero section developed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        Felipe's Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a Frontend Engineer with a Computer Engineering Degree and currently working at Compass.UOL
+      </SectionText>
+      <Button onClick={() => window.location.href = "mailto:feliperubenmv@gmail.com"}>
+        Talk to me
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
