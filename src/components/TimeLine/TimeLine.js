67975fe8a3bf099e8791b133e7f1a4f75e6f7f5a
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-11 20:13:06 -0300

    removing gradient because safari cant handle it

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -74,22 +74,6 @@ const Timeline = () => {
                       fill="url(#paint0_linear)"
                       fill-opacity="0.33"
                     />
-                    <defs>
-                      <linearGradient
-                        id="paint0_linear"
-                        x1="-4.30412e-10"
-                        y1="0.5"
-                        x2="208"
-                        y2="0.500295"
-                        gradientUnits="userSpaceOnUse">
-                        <stop stop-color="white" />
-                        <stop
-                          offset="0.79478"
-                          stop-color="white"
-                          stop-opacity="0"
-                        />
-                      </linearGradient>
-                    </defs>
                   </CarouselItemImg>
                 </CarouselItemTitle>
                 <CarouselItemText>{item.text}</CarouselItemText>
