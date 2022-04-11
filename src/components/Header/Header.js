a44758bbc0e6ef9b349a960b86050c9eb5c6cdac
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-11 19:56:23 -0300

    scroll fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,44 +5,56 @@ import { DiCssdeck } from 'react-icons/di';
 
 import { NavbarContainer, Div1, Div2, Div3, NavLink, SocialIcons, Span, ATag } from './HeaderStyles';
 
-const Header = () =>  (
-  <NavbarContainer>
-    <Div1>
-      <Link href="/">
-        <ATag>
-          <DiCssdeck size="3rem" /> <Span>Felipe Cunha</Span>
-        </ATag>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
+const Header = () =>  {
+  const scrollToHash = props => {
+    const element = document.querySelector(`#${props}`);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      });
+    }
+  };
+
+  return (
+    <NavbarContainer>
+      <Div1>
+        <Link href="/">
+          <ATag>
+            <DiCssdeck size="3rem" /> <Span>Felipe Cunha</Span>
+          </ATag>
         </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons target="_blank" href="https://github.com/feliperucunha">
-        <AiFillGithub size={30} />
-      </SocialIcons>
-      <SocialIcons target="_blank" href="https://www.linkedin.com/in/feliperubencunha/">
-        <AiFillLinkedin size={30} />
-      </SocialIcons>
-      <SocialIcons target="_blank" href="https://www.instagram.com/feliperubeen">
-        <AiFillInstagram size={30} />
-      </SocialIcons>
-    </Div3>
-  </NavbarContainer>
-);
+      </Div1>
+      <Div2>
+        <li>
+          <a onClick={() => scrollToHash('projects')}>
+            <NavLink>Projects</NavLink>
+          </a>
+        </li>
+        <li>
+          <a onClick={() => scrollToHash('tech')}>
+            <NavLink>Technologies</NavLink>
+          </a>
+        </li>
+        <li>
+          <a onClick={() => scrollToHash('about')}>
+            <NavLink>About</NavLink>
+          </a>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons target="_blank" href="https://github.com/feliperucunha">
+          <AiFillGithub size={30} />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://www.linkedin.com/in/feliperubencunha/">
+          <AiFillLinkedin size={30} />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://www.instagram.com/feliperubeen">
+          <AiFillInstagram size={30} />
+        </SocialIcons>
+      </Div3>
+    </NavbarContainer>
+  )
+};
 
 export default Header;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,9 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <Section nopadding id="projects">
+  <Section nopadding >
     <SectionDivider />
-    <SectionTitle main>
+    <SectionTitle main id="projects">
       Personal Projects
     </SectionTitle>
     <GridContainer>
